19d27d57dd685bac316cffdd51e70715b3ffa1b8
Viet Tai <viettaixca123@gmail.com>
2023-08-03 08:27:14 +0700

    Deploy vercel

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ const express = require("express");
 const app = express();
 
 //connectDB
-const connectDB = require("./db/connect");
-const PORT =  4000;
+const PORT = 4000;
+
+app.get("/home", (req, res) => {
+  res.send("<h1>Home</h1>");
+});
+
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
     app.listen(PORT, () =>
       console.log(`Server is listening on port ${PORT}...`)
     );
